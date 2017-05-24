0dbadd1a269563657d8e6ff6c335a40085110f13
Rob <robmoore121@gmail.com>
2017-05-24 22:48:47 +0100

    Optimizing imports/removing noise

--- PhoneMapAndroid/app/src/main/java/com/phonemap/phonemap/MainActivity.java
+++ PhoneMapAndroid/app/src/main/java/com/phonemap/phonemap/MainActivity.java
@@ -1,14 +1,12 @@
 package com.phonemap.phonemap;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Looper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import com.phonemap.phonemap.services.Controller;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
