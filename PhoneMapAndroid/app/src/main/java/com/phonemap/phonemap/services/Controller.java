baaa1d53cc5932de6ae264e81ab6a1319c775708
Martin Zlocha <mz4315@ic.ac.uk>
2017-05-25 17:38:59 +0100

    Implemented basic sockets - will refactor now.

--- PhoneMapAndroid/app/src/main/java/com/phonemap/phonemap/services/Controller.java
+++ PhoneMapAndroid/app/src/main/java/com/phonemap/phonemap/services/Controller.java
@@ -2,19 +2,82 @@ package com.phonemap.phonemap.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.EmbossMaskFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
 import java.net.URL;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class Controller extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        try {
+            final Socket socket = IO.socket("http://146.169.45.121:5000/test");
+
+            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.i("LOG", "Connected");
+                    JSONObject object = new JSONObject();
+                    try {
+                        object.put("data", "Sup");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    socket.emit("my_broadcast_event", object);
+                    socket.emit("my_broadcast_event", object);
+                }
+            });
+
+            socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.i("LOG", "Disconnected");
+                }
+            });
+
+            socket.on(Socket.EVENT_MESSAGE, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.i("LOG", String.valueOf(args.length));
+                }
+            });
+
+            socket.on(Socket.EVENT_ERROR, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.i("LOG", "We fucked up");
+                    Log.i("LOG", String.valueOf(args.length));
+                    Log.i("LOG", String.valueOf(args[0]));
+                }
+            });
+
+            socket.on(Socket.EVENT_PING, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.i("LOG", "Ping");
+                }
+            });
+
+            socket.connect();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
         startService(new Intent(getApplicationContext(), JSRunner.class));
         return Service.START_NOT_STICKY;
     }
