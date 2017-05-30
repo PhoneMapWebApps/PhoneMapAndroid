0de3c0bd06fcaafc25f58b3e95a72364582d4d2e
Martin Zlocha <mz4315@ic.ac.uk>
2017-05-30 15:51:30 +0100

    Created some tests for JSRunner.

--- PhoneMapAndroid/app/src/test/java/com/phonemap/phonemap/services/JSRunnerTest.java
+++ PhoneMapAndroid/app/src/test/java/com/phonemap/phonemap/services/JSRunnerTest.java
@@ -1,5 +1,49 @@
 package com.phonemap.phonemap.services;
 
 
+import android.net.Uri;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+
 public class JSRunnerTest {
+
+    JSRunner runner;
+
+    @Before
+    public void setup() {
+        runner = new JSRunner();
+    }
+
+    @Test
+    public void testConvertPathToURIFailsOnEmptyString() {
+        try {
+            runner.convertPathToURI("");
+        } catch (URISyntaxException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+    public void testConvertPathToURIDoesNotFailOnFilename() {
+        URI uri;
+
+        try {
+            uri = runner.convertPathToURI("code.js");
+        } catch (URISyntaxException e) {
+            fail();
+            return;
+        }
+
+        assertTrue(uri.toString().contains("code.js"));
+        assertTrue(uri.toString().contains("file://"));
+    }
 }
