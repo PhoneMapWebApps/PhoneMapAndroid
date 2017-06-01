8ac9daa481354592d3df9a3fda08ea9d08bb3171
Martin Zlocha <mz4315@ic.ac.uk>
2017-06-01 19:45:29 +0100

    Set notify=0 to stop annoying emails.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 curl \
 -F "status=2" \
--F "notify=1" \
+-F "notify=0" \
 -F "ipa=@app/build/outputs/apk/app-debug.apk" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
 https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
