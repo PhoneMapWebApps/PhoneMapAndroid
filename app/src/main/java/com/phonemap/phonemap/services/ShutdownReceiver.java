2387b9a90989af67e214f353e7fe108e83e28cea
Rob <robmoore121@gmail.com>
2017-06-05 16:55:36 +0100

    little refactor

--- app/src/main/java/com/phonemap/phonemap/services/ShutdownReceiver.java
+++ app/src/main/java/com/phonemap/phonemap/services/ShutdownReceiver.java
@@ -20,13 +20,10 @@ public class ShutdownReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        String intendedAction = intent.getAction();
         if  (service == null) {
             Log.i(LOG_TAG, "Wanted to send onDestroy but service isn't running");
-            return;
-        }
-
-        String intendedAction = intent.getAction();
-        if (intendedAction.equals(Intent.ACTION_SHUTDOWN)) {
+        } else if (intendedAction.equals(Intent.ACTION_SHUTDOWN)) {
             service.emit(ON_DESTROY, true);
         } else if (intendedAction.equals(Intent.ACTION_SCREEN_ON) || intendedAction.equals(Intent.ACTION_POWER_DISCONNECTED)) {
             service.emit(ON_DESTROY, false);
