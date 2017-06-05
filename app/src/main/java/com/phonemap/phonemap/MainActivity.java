4818013a61ce33cc2f30b439325d2b852865c8a5
Martin Zlocha <mz4315@ic.ac.uk>
2017-06-05 13:44:27 +0200

    Removed old UI to allow creation of a new user friendly one.

--- app/src/main/java/com/phonemap/phonemap/MainActivity.java
+++ app/src/main/java/com/phonemap/phonemap/MainActivity.java
@@ -7,27 +7,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.TextView;
 
 import com.phonemap.phonemap.services.JSRunner;
 
-import java.text.DateFormat;
-import java.util.Date;
-
 import static com.phonemap.phonemap.constants.Intents.JSRUNNER_STARTED_INTENT;
 import static com.phonemap.phonemap.constants.Intents.JSRUNNER_STOP_INTENT;
-import static com.phonemap.phonemap.constants.Preferences.PREFERENCES;
-import static com.phonemap.phonemap.constants.Preferences.RUN_AUTOMATICALLY;
 
 public class MainActivity extends AppCompatActivity {
-
-    private SharedPreferences settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         registerIntentFilter();
-
-        settings = getSharedPreferences(PREFERENCES, 0);
-        boolean automatically = settings.getBoolean(RUN_AUTOMATICALLY, false);
-        CheckBox checkbox = (CheckBox) findViewById(R.id.checkBox);
-        checkbox.setChecked(automatically);
-
-        if (automatically) {
-            startService(new Intent(this, JSRunner.class));
-        }
-
+        startService(new Intent(this, JSRunner.class));
         checkForUpdates();
     }
 
@@ -66,19 +46,6 @@ public class MainActivity extends AppCompatActivity {
         unregisterManagers();
     }
 
-    public void onStartClick(View v) {
-        CheckBox checkbox = (CheckBox) findViewById(R.id.checkBox);
-        Boolean checked = checkbox.isChecked();
-
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putBoolean(RUN_AUTOMATICALLY, checked);
-        editor.apply();
-
-        if (checked) {
-            startService(new Intent(this, JSRunner.class));
-        }
-    }
-
     private void registerIntentFilter() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(JSRUNNER_STOP_INTENT);
@@ -90,21 +57,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(JSRUNNER_STOP_INTENT)) {
-                logStatus("Stopped");
+                // JSRunenr stopped
             } else if (intent.getAction().equals(JSRUNNER_STARTED_INTENT)) {
-                logStatus("Started");
+                // JSRunner started executing new task
             }
         }
     };
-
-    private void logStatus(String status) {
-        final TextView textView = (TextView) findViewById(R.id.textView);
-
-        String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
-        String log = currentDateTimeString + ": " + status + "\n" + textView.getText();
-
-        textView.setText(log);
-    }
 
 
     private void checkForCrashes() {
