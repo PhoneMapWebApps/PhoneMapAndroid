7bfabf0f9cd72aba2a835f3cb28b5af18d04d6c2
Paul Larsen <pn1315@ic.ac.uk>
2017-06-14 11:07:07 +0100

    update namespace

--- app/src/main/java/com/phonemap/phonemap/constants/Server.java
+++ app/src/main/java/com/phonemap/phonemap/constants/Server.java
@@ -5,7 +5,7 @@ import java.net.URI;
 public class Server {
     public static final String PROTOCOL = "http";
     public static final String IP = "146.169.45.121";
-    public static final String WS_NAMESPACE = "test";
+    public static final String WS_NAMESPACE = "phone";
 
     public static final String WS_URL = PROTOCOL + "://" + IP  + "/" + WS_NAMESPACE;
     public static final URI WS_URI = URI.create(WS_URL);
